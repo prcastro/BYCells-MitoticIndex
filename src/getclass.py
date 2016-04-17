e220e1922cb66c0cc2f5ea72536515d3e6a00d4e
EstevaoVieira <estevovsky@gmail.com>
2016-04-17 17:55:59 -0300

    Adding .tif to dictionary

--- src/getclass.py
+++ src/getclass.py
@@ -1,12 +1,15 @@
-classifs = {}
 import pickle
 import os
 
-
-with open('../data/Classificação') as f:
-    head = [f.readline() for i in range(10)]
+#if os.path.isfile('classdict.p'):
+#    classifs = pickle.load(open('classdict.p', 'rb'))
+#else:
+#    classifs = {}
+classifs={}
+with open('../data/classification') as f:
+    head = [f.readline() for i in range(9)]
     for line in f:
         linesp = line.split()
-        classifs[linesp[0]]= linesp[2]
+        classifs[linesp[0]+'.tif'] = linesp[2]
 
 pickle.dump(classifs, open("classdict.p", "wb"))
